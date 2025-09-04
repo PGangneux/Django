40e31b0e8fd20045cda5a2971315f67797d0f40c
Gangneux Pierre <o22300799@or-iut-i102-f22.iut45.univ-orleans.fr>
2025-09-04 11:15:38 +0200

    td1 fini

--- TutoDjango/monApp/views.py
+++ TutoDjango/monApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request, param=None):
+    if param:
+        return HttpResponse(f"<h1>Hello Django!</h1><p>Bonjour {param}</p>")
+    else:
+        return HttpResponse("<h1>Hello Django!</h1>")
+
+def contact(request):
+    return HttpResponse("<h1>contact us</h1>")
+
+def aboutUs(request):
+    return HttpResponse("<h1>about us</h1>")
