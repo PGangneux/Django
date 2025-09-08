34563a42f4d1915468a793903d18de8f5c672984
GangneuxPierre <pierre.gangneux@etu.univ-orleans.fr>
2025-09-08 15:28:02 +0200

    je viens d'installer django toolbar

--- TutoDjango/monApp/admin.py
+++ TutoDjango/monApp/admin.py
@@ -1,3 +1,67 @@
+from decimal import ROUND_HALF_UP, Decimal
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+class ProduitFilter(admin.SimpleListFilter):
+    title = 'filtre produit'
+    parameter_name = 'custom_status'
+
+    def lookups(self, request, model_admin) :
+        return (
+            ('OnLine', 'En ligne'),
+            ('OffLine', 'Hors ligne'),
+        )
+    
+    def queryset(self, request, queryset):
+        if self.value() == 'OnLine':
+            return queryset.filter(status=1)
+        if self.value() == 'OffLine':
+            return queryset.filter(status=0)
+        
+def set_Produit_online(modeladmin, request, queryset):
+    queryset.update(status=1)
+set_Produit_online.short_description = "Mettre en ligne"
+
+
+def set_Produit_offline(modeladmin, request, queryset):
+    queryset.update(status=0)
+set_Produit_offline.short_description = "Mettre hors ligne"
+        
+
+class ProduitAdmin(admin.ModelAdmin):
+    model = Produit
+    list_display = ["refProd", "intituleProd", "prixUnitaireProd", "dateFabProd", "categorie", "status"]
+    list_editable = ["intituleProd", "prixUnitaireProd"]
+    radio_fields = {"status": admin.VERTICAL}
+    search_fields = ('intituleProd', 'dateFabProd')
+    list_filter = (ProduitFilter,)
+    date_hierarchy = 'dateFabProd'
+    ordering = ('-dateFabProd',)
+    actions = [set_Produit_online, set_Produit_offline]
+
+    def prixTTCProd(self, instance):
+        return (instance.prixUnitaireProd * Decimal('1.20')).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+    prixTTCProd.short_description = "Prix TTC"
+    prixTTCProd.admin_order_field = "prixUnitaireProd"
+
+
+
+
+class ProduitInline(admin.TabularInline):
+    model = Produit
+    extra = 1 # nombre de lignes vides par défaut
+
+
+class CategorieAdmin(admin.ModelAdmin):
+    model = Categorie
+    inlines = [ProduitInline]
+
+
+admin.site.register(Produit, ProduitAdmin)
+admin.site.register(Categorie)
+admin.site.register(Statut)
+admin.site.register(Rayon)
+admin.site.register(Contenir)
--- TutoDjango/monApp/models.py
+++ TutoDjango/monApp/models.py
@@ -1,34 +1,35 @@
 from datetime import date
 from django.db import models
 
-
-class Statut(models.Model):
-    idStatut = models.AutoField(primary_key=True)
-    libelle = models.CharField(max_length=200)
-    
-    
-    def __str__(self):
-        return self.libelle
-
+# Create your models here.
 class Categorie(models.Model):
     idCat = models.AutoField(primary_key=True)
     nomCat = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nomCat
+
+class Statut(models.Model):
+    idStatus = models.AutoField(primary_key=True)
+    libelleStatus = models.CharField(max_length=100, default='')
+          
+    def __str__(self):
+        return self.libelleStatus
     
+
 class Produit(models.Model):
     refProd = models.AutoField(primary_key=True)
     intituleProd = models.CharField(max_length=200)
     prixUnitaireProd = models.DecimalField(max_digits=10, decimal_places=2)
-    dateFabrication = models.DateField(default=date.today)
-    # Relation CIF : chaque produit appartient à 1 catégorie (0,N côté catégorie 1,1 côté produit)→
-    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE, related_name="produits",null=True, blank=True)
-    statut = models.ForeignKey(Statut, on_delete=models.CASCADE, related_name="produits",null=True, blank=True)
+    dateFabProd = models.DateField(default=date.today)
+
+    # Relation CIF : chaque produit appartient à 1 catégorie (0,N côté catégorie → 1,1 côté produit)
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE, related_name="produits_categorie", null=True, blank=True)
+    status = models.ForeignKey(Statut, on_delete=models.CASCADE, related_name="produits_status",null=True, blank=True)
 
     def __str__(self):
         return self.intituleProd
-    
+
 class Rayon(models.Model):
     idRayon = models.AutoField(primary_key=True)
     nomRayon = models.CharField(max_length=100)
@@ -37,13 +38,13 @@ class Rayon(models.Model):
         return self.nomRayon
     
 class Contenir(models.Model):
-    qte = models.FloatField()
+    produit = models.ForeignKey(Produit, on_delete=models.CASCADE, related_name="contenir_produit")
+    rayon = models.ForeignKey(Rayon, on_delete=models.CASCADE, related_name="contenir_rayon")
+    Qte = models.PositiveIntegerField(default=0)
 
-    produit = models.ForeignKey(Produit, on_delete=models.CASCADE, related_name="contenir",null=True, blank=True)
-    rayon = models.ForeignKey(Rayon, on_delete=models.CASCADE, related_name="contenir",null=True, blank=True)
-    
+    class Meta:
+        unique_together = ('produit', 'rayon')  # Un produit dans un rayon spécifique est unique
+
     def __str__(self):
-        return self.qte
+        return f"{self.produit} dans {self.rayon} (Qte: {self.Qte})"
     
-    
-
